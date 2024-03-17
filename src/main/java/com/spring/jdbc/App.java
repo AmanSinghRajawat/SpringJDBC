92f05ee2f19700509eb3199ee17accf3dc94bd1c
AmanSinghRajawat <iamamansingh17@gmail.com>
2024-03-17 08:49:00 +0530

    first commit

---
+++ src/main/java/com/spring/jdbc/App.java
@@ -0,0 +1,36 @@
+package com.spring.jdbc;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        
+        ApplicationContext context = new ClassPathXmlApplicationContext("com/spring/jdbc/config.xml");
+        
+        JdbcTemplate jdbc = context.getBean("jdbcTemp", JdbcTemplate.class);
+        
+        //insert query  -> using values
+         String q1 = "insert into student values (333,'chaman', 'agra')";
+         //execute query
+         int result1 = jdbc.update(q1);
+
+         System.out.println("update result1 "+result1);
+         
+         // insert query  ->  using question mark
+         String s2 = "insert into student values(?,?,?)";
+         
+         int result2 = jdbc.update(s2, 444,"rahul","delhi");
+         System.out.println("update result2 "+result2);
+        
+    }
+}
---
+++ src/main/java/com/spring/jdbc/Student.java
@@ -0,0 +1,51 @@
+package com.spring.jdbc;
+
+public class Student {
+
+	private long rollNo;
+	private String name;
+	private String city;
+	
+	
+	public long getRollNo() {
+		return rollNo;
+	}
+	public void setRollNo(long rollNo) {
+		this.rollNo = rollNo;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	
+	public Student(long rollNo, String name, String city) {
+		super();
+		this.rollNo = rollNo;
+		this.name = name;
+		this.city = city;
+	}
+	
+	
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Student [rollNo=" + rollNo + ", name=" + name + ", city=" + city + "]";
+	}
+	
+	
+	
+}
